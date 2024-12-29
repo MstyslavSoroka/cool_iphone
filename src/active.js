8b73a2da0cd1f9b2c0c0c6d39830c45e99e5904c
Mstyslav Soroka <mstyslav2010@gmail.com>
2024-12-29 11:13:04 +0000

    added some styles

---
+++ src/active.js
@@ -0,0 +1,15 @@
+function manageSingleActive(groupClass) {
+  const groupButtons = document.querySelectorAll(`.${groupClass}`);
+
+  groupButtons.forEach((button) => {
+    button.addEventListener('click', () => {
+      groupButtons.forEach((btn) => btn.classList.remove('active'));
+
+      button.classList.add('active');
+    });
+  });
+}
+
+manageSingleActive('gigabyte');
+
+manageSingleActive('sim');
