7171a35ee5915711ef92712040763ea62984f765
Mstyslav Soroka <mstyslav2010@gmail.com>
2024-12-30 19:43:40 +0000

    fix

--- docs/color-change.js
+++ docs/color-change.js
@@ -1,63 +1,27 @@
-import blackFront from './img/black/front.jpg';
-import blackSide from './img/black/side.jpg';
-import blackTilted from './img/black/tilted.jpg';
-import blackFrontBack from './img/black/front-back.jpg';
+// Select buttons for each color
+const black = document.querySelector('.black');
+const natural = document.querySelector('.natural');
+const white = document.querySelector('.white');
+const blue = document.querySelector('.blue');
 
-import naturalFront from './img/natural/front.jpg';
-import naturalSide from './img/natural/side.jpg';
-import naturalTilted from './img/natural/tilted.jpg';
-import naturalFrontBack from './img/natural/front-back.jpg';
+// Select the image elements
+const front = document.querySelector('.front-img');
+const tilted = document.querySelector('.tilted');
+const side = document.querySelector('.side-img');
+const frontBack = document.querySelector('.front-back');
+const chosen = document.querySelector('.chosen');
 
-import whiteFront from './img/white/front.jpg';
-import whiteSide from './img/white/side.jpg';
-import whiteTilted from './img/white/tilted.jpg';
-import whiteFrontBack from './img/white/front-back.jpg';
-
-import blueFront from './img/blue/front.jpg';
-import blueSide from './img/blue/side.jpg';
-import blueTilted from './img/blue/tilted.jpg';
-import blueFrontBack from './img/blue/front-back.jpg';
-
-const updateImages = (colorImages) => {
-  document.querySelector('.front-img').src = colorImages.front;
-  document.querySelector('.side-img').src = colorImages.side;
-  document.querySelector('.tilted').src = colorImages.tilted;
-  document.querySelector('.front-back').src = colorImages.frontBack;
-  document.querySelector('.chosen').src = colorImages.frontBack;
+// Function to update images dynamically based on selected color
+const updateImages = (color) => {
+  front.src = `./img/${color}/front.jpg`;
+  side.src = `./img/${color}/side.jpg`;
+  tilted.src = `./img/${color}/tilted.jpg`;
+  frontBack.src = `./img/${color}/front-back.jpg`;
+  chosen.src = `./img/${color}/front-back.jpg`;
 };
 
-document.querySelector('.black').addEventListener('click', () => {
-  updateImages({
-    front: blackFront,
-    side: blackSide,
-    tilted: blackTilted,
-    frontBack: blackFrontBack,
-  });
-});
-
-document.querySelector('.natural').addEventListener('click', () => {
-  updateImages({
-    front: naturalFront,
-    side: naturalSide,
-    tilted: naturalTilted,
-    frontBack: naturalFrontBack,
-  });
-});
-
-document.querySelector('.white').addEventListener('click', () => {
-  updateImages({
-    front: whiteFront,
-    side: whiteSide,
-    tilted: whiteTilted,
-    frontBack: whiteFrontBack,
-  });
-});
-
-document.querySelector('.blue').addEventListener('click', () => {
-  updateImages({
-    front: blueFront,
-    side: blueSide,
-    tilted: blueTilted,
-    frontBack: blueFrontBack,
-  });
-});
+// Add event listeners for each color
+black.addEventListener('click', () => updateImages('black'));
+natural.addEventListener('click', () => updateImages('natural'));
+white.addEventListener('click', () => updateImages('white'));
+blue.addEventListener('click', () => updateImages('blue'));
