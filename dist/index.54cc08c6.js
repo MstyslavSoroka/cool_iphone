2a55674e65d7a5b60d8987bc55e17f4bae79f8d7
Mstyslav Soroka <mstyslav2010@gmail.com>
2025-01-01 13:46:46 +0000

    added local color change

--- dist/index.54cc08c6.js
+++ dist/index.54cc08c6.js
@@ -2,17 +2,21 @@ const black = document.querySelector('.black');
 const natural = document.querySelector('.natural');
 const white = document.querySelector('.white');
 const blue = document.querySelector('.blue');
-const front = document.querySelector('.front');
+const front = document.querySelector('.front-img');
 const tilted = document.querySelector('.tilted');
-const side = document.querySelector('.side');
+const side = document.querySelector('.side-img');
 const frontBack = document.querySelector('.front-back');
 const chosen = document.querySelector('.chosen');
-black.addEventListener('click', ()=>{
-    front.src = 'img/black/front.jpg';
-    side.src = 'img/black/side.jpg';
-    tilted.src = 'img/black/tilted.jpg';
-    frontBack.src = 'img/black/front-back.jpg';
-    chosen.src = 'img/black/front-back.jpg';
-});
+const updateImages = (color)=>{
+    front.src = `./img/${color}/front.jpg`;
+    side.src = `./img/${color}/side.jpg`;
+    tilted.src = `./img/${color}/tilted.jpg`;
+    frontBack.src = `./img/${color}/front-back.jpg`;
+    chosen.src = `./img/${color}/front-back.jpg`;
+};
+black.addEventListener('click', ()=>updateImages('black'));
+natural.addEventListener('click', ()=>updateImages('natural'));
+white.addEventListener('click', ()=>updateImages('white'));
+blue.addEventListener('click', ()=>updateImages('blue'));
 
 //# sourceMappingURL=index.54cc08c6.js.map
---
+++ docs/local-color-change.js
@@ -0,0 +1,64 @@
+import blackFront from './img/black/front.jpg';
+import blackSide from './img/black/side.jpg';
+import blackTilted from './img/black/tilted.jpg';
+import blackFrontBack from './img/black/front-back.jpg';
+import naturalFront from './img/natural/front.jpg';
+import naturalSide from './img/natural/side.jpg';
+import naturalTilted from './img/natural/tilted.jpg';
+import naturalFrontBack from './img/natural/front-back.jpg';
+import whiteFront from './img/white/front.jpg';
+import whiteSide from './img/white/side.jpg';
+import whiteTilted from './img/white/tilted.jpg';
+import whiteFrontBack from './img/white/front-back.jpg';
+import blueFront from './img/blue/front.jpg';
+import blueSide from './img/blue/side.jpg';
+import blueTilted from './img/blue/tilted.jpg';
+import blueFrontBack from './img/blue/front-back.jpg';
+const black = document.querySelector('.black');
+const natural = document.querySelector('.natural');
+const white = document.querySelector('.white');
+const blue = document.querySelector('.blue');
+const front = document.querySelector('.front-img');
+const tilted = document.querySelector('.tilted');
+const side = document.querySelector('.side-img');
+const frontBack = document.querySelector('.front-back');
+const chosen = document.querySelector('.chosen');
+const updateImages = (colorImages) => {
+  front.src = colorImages.front;
+  side.src = colorImages.side;
+  tilted.src = colorImages.tilted;
+  frontBack.src = colorImages.frontBack;
+  chosen.src = colorImages.frontBack;
+};
+black.addEventListener('click', () => {
+  updateImages({
+    front: blackFront,
+    side: blackSide,
+    tilted: blackTilted,
+    frontBack: blackFrontBack,
+  });
+});
+natural.addEventListener('click', () => {
+  updateImages({
+    front: naturalFront,
+    side: naturalSide,
+    tilted: naturalTilted,
+    frontBack: naturalFrontBack,
+  });
+});
+white.addEventListener('click', () => {
+  updateImages({
+    front: whiteFront,
+    side: whiteSide,
+    tilted: whiteTilted,
+    frontBack: whiteFrontBack,
+  });
+});
+blue.addEventListener('click', () => {
+  updateImages({
+    front: blueFront,
+    side: blueSide,
+    tilted: blueTilted,
+    frontBack: blueFrontBack,
+  });
+});
